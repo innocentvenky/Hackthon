098408fad83d2fd9c07f8d5e73f3a5faf9582e78
jeswanth <venkateshjaripiti123@gmail.com>
2026-07-01 13:48:03 +0530

    Configure static files for deployment

--- python_hackthon/python_hackthon/settings.py
+++ python_hackthon/python_hackthon/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9d7$!7tundn#o(ys&fb$6=ue4tp^65dcahyms8(r*e%2y7^s93'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
