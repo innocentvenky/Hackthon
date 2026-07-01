deaeadd1d91bb56870e3aed7a6709eb54e1151e1
jeswanth <venkateshjaripiti123@gmail.com>
2026-07-01 13:44:24 +0530

    Configure static files for deployment

--- python_hackthon/python_hackthon/settings.py
+++ python_hackthon/python_hackthon/settings.py
@@ -24,13 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9d7$!7tundn#o(ys&fb$6=ue4tp^65dcahyms8(r*e%2y7^s93'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = [
-    ".up.railsway.app",
-    "hackathon-production-745e.up.railway.app",
+    "https://*.up.railway.app",
+    "https://hackathon-production-745e.up.railway.app",
 ]
 # Application definition
 
